e5993168c5536b4f2319f4e89874160cbed94460
jeunir23 <jeunir23@gmail.com>
2024-06-03 21:28:28 +0800

    rename class button to button hidden

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelector('.show-modal');
+const clickButton = document.querySelector('.button');
 
-const op
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
